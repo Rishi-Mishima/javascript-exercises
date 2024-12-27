2bfc0e439ea0c6ae21cdb26eba4393bdac38a222
Rishi-Mishima <janicerx10@gmail.com>
2024-12-27 20:49:11 +0100

    02 finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    // note: the if should be put b4 the for loop
+    if (num < 0) {
+
+        return 'ERROR'
+    }
+
+    let result = '';
+    for (let i = 0; i < num; i++) {
+
+        result = result + string
+    }
+    return result
+
 
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
