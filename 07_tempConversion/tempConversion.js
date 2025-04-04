f41d7580f3bde11a7f11dd275f99918d7c092b6b
Rishi-Mishima <153814332+Rishi-Mishima@users.noreply.github.com>
2025-04-05 01:59:05 +0200

    07 finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+
+  //x °C ≘ (x × ⁠9/5+ 32) °F
+
+  //x °F ≘ (x − 32) × ⁠5/9°C
+
+  let c = 0; 
+
+  c= Math.round((Fahrenheit -32) *(5/9)*10)/10; 
+
+
+  return c; 
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Celsius) {
+
+  let f = 0; 
+  f= Math.round((Celsius * 9 / 5 + 32)*10)/10; 
+  return f;
 };
 
 // Do not edit below this line
