80b83514290cfeec87f1c7b7f75a121401954a5e
Rishi-Mishima <janicerx10@gmail.com>
2025-04-04 22:54:45 +0200

    04 finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...values) {
+
+    const result = []
+
+    for (let i = 0; i < arr.length; i++) {
+        // Check if the current element is NOT in the `values` array
+        if (!values.includes(arr[i])) {
+            result.push(arr[i]); // Add elements that are NOT in `values`
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
